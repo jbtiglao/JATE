bfc24b9f8671932d8649d853be184131663efa3c
Jane Tiglao <jane.b.tiglao@gmail.com>
2022-06-08 05:26:25 -0700

    updated package.json script to correct heroku deployment error

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -31,7 +31,7 @@ module.exports = () => {
         inject: true,
         name: 'Just Another Text Editor',
         short_name: 'J.A.T.E.',
-        description: "This application installs 'Just Another Text Editor' or 'JATE' for short. Clever?",
+        description: "This application installs 'Just Another Text Editor' or 'JATE' for short.",
         background_color: '#225ca3',
         theme_color: '#225ca3',
         start_url: '/',
